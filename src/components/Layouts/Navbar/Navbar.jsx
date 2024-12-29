d8d656b1bc067559b77b8c1b6907e004ffe8546d
asmaa905 <asmaa_abdelnaser38@yahoo.com>
2024-12-29 23:20:48 +0200

    etest

--- src/components/Layouts/Navbar/Navbar.jsx
+++ src/components/Layouts/Navbar/Navbar.jsx
@@ -144,7 +144,7 @@ export default function Navbar() {
                         <li>
                           <Link
                             className="hover:bg-[#ccc] w-full px-[10px] py-[5px]"
-                            to="/all"
+                            to="/allorders"
                             onClick={() => {
                               toggleDropdown();
                             }}
